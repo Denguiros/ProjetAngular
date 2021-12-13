45801a3751fe0366d91ff62bdc69d7a130657b70
Soufiane Ben Yaala <soufianebenyaala1998@gmail.com>
2021-12-13 15:53:54 +0100

    add calender

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -9,32 +9,40 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent implements OnInit {
-  Events = [];
-  calendarOptions!: CalendarOptions;
+
+  Events: any = [];
+
+  calendarOptions: CalendarOptions = {};
 
   constructor(private httpClient: HttpClient) { }
 
-  onDateClick(res) {
-    alert('Clicked on date : ' + res.dateStr)
+
+  onDateSelect(arg: any) {
+    alert('Date clicked: ' + arg.dateStr)
   }
 
   ngOnInit(){
-    setTimeout(() => {
-      return this.httpClient.get('http://localhost:8888/event.php')
-        .subscribe(res => {
-            this.Events.push(res);
-            console.log(this.Events);
-        });
-    }, 2200);
+      setTimeout(() => {
+          return this.httpClient.get('http://localhost/test/events.php')
+          .subscribe((res: any) => {
+            console.log(res.start.toLocaleString());
+              this.Events.push(res);
+              console.log(this.Events);
+          });
+      }, 0);
 
-    setTimeout(() => {
-      this.calendarOptions = {
-        initialView: 'dayGridMonth',
-        dateClick: this.onDateClick.bind(this),
-        events: this.Events
-      };
-    }, 2500);
-
-    }
+      setTimeout(() => {
+          this.calendarOptions = {
+          initialView: 'dayGridMonth',
+          headerToolbar: {
+            left: 'prev,next today',
+            center: 'title',
+            right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+          },
+          dateClick: this.onDateSelect.bind(this),
+          events: this.Events
+          };
+      }, 500);
+  }
 
 }
